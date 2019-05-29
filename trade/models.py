2b4250fc3038f816affc41edaa2b5a36102e9c7e
dokeey <ehdgnv@naver.com>
2019-05-29 19:44:30 +0900

    add: Item 모델 생성, Pillow 설치

--- trade/models.py
+++ trade/models.py
@@ -1,3 +1,20 @@
+from django.conf import settings
 from django.db import models
+from category.models import Category, SubCategory
 
 # Create your models here.
+
+
+class Item(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    desc = models.TextField(blank=True)
+    amount = models.PositiveIntegerField()
+    photo = models.ImageField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    sub_category = models.ForeignKey(SubCategory, on_delete=models.CASCADE)
+    status = models.CharField(max_length=10)
+    photo = models.ImageField()
+    is_public = models.BooleanField(default=False, db_index=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
