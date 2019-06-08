e44e19b3a929e914742b4d08e98dbbded6ae6adc
dokeey <ehdgnv@naver.com>
2019-06-08 13:36:08 +0900

    add: WishList 모델 생성

--- mypage/models.py
+++ mypage/models.py
@@ -1,3 +1,10 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
+from trade.models import Item
+
+
+class WishList(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
