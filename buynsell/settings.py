d5591af62cdf70e65f297b3ece342ce70a3bc6c9
dokeey <ehdgnv@naver.com>
2019-06-13 03:59:01 +0900

    add: bootstrap 3.4v custom navbar 적용

--- buynsell/settings.py
+++ buynsell/settings.py
@@ -138,6 +138,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 AUTH_USER_MODEL = 'accounts.User'
 
