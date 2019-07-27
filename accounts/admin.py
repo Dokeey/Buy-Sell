b6b9275b50410d339b226679af17abdf3b652417
dokeey <ehdgnv@naver.com>
2019-07-27 19:59:52 +0900

    mod: accounts Admin 페이지 Store 연결/iamport.js 로컬저장소 추가

--- accounts/admin.py
+++ accounts/admin.py
@@ -4,44 +4,85 @@ from django.contrib.auth import get_user_model
 
 from django.contrib.auth.models import Permission
 from django.db.models import Count
+from django.urls import reverse
+from django.utils.encoding import force_text
 from django.utils.safestring import mark_safe
 
-from store.models import StoreProfile
+
+from store.models import StoreProfile, QuestionComment, StoreGrade
 
 from .forms import SignupForm
 from .models import UserSession, Profile
 
 admin.site.register(Permission)
 
+
+def store_image(obj):
+
+    if type(obj) == get_user_model():
+        link = reverse('admin:store_storeprofile_change', args=[force_text(obj.storeprofile.pk)])
+        return mark_safe('<a href="{link}"><img src="{url}" width="{width}" height={height} /></a>'.format(
+            link=link,
+            url=obj.storeprofile.photo.url,
+            width=100,
+            height=100,
+        ))
+    else:
+        link = reverse('admin:store_storeprofile_change', args=[force_text(obj.pk)])
+        return mark_safe('<a href="{link}"><img src="{url}" width="{width}" height={height} /></a>'.format(
+            link=link,
+            url=obj.photo.url,
+            width=100,
+            height=100,
+        ))
+store_image.short_description = '스토어 사진 뷰'
+
+
+class QuestionCommentAdmin(admin.StackedInline):
+    model = QuestionComment
+    extra = 0
+    fields = ['author', 'comment', 'updated_at']
+    readonly_fields = ['author', 'updated_at']
+
+class StoreGradeAdmin(admin.StackedInline):
+    model = StoreGrade
+    extra = 0
+    fields = ['store_item', 'rating', 'author', 'grade_comment', 'updated_at']
+    readonly_fields = ['store_item', 'rating', 'author', 'updated_at']
+
 class ProfileInline(admin.StackedInline):
     model = Profile
 
 class StoreProfileInline(admin.StackedInline):
     model = StoreProfile
-    readonly_fields = ['store_image']
+    fields = ['name', store_image, 'photo', 'comment','get_edit_link']
+    readonly_fields = [store_image,'get_edit_link']
 
-    def store_image(self, obj):
-        return mark_safe('<img src="{url}" width="{width}" height={height} />'.format(
-            url = obj.photo.url,
-            width=100,
-            height=100,
-            )
-    )
-    store_image.short_description = '스토어 사진 뷰'
+    def get_edit_link(self, obj=None):
+        if obj.pk:  # if object has already been saved and has a primary key, show link to it
+            url = reverse('admin:%s_%s_change' % (obj._meta.app_label, obj._meta.model_name), args=[force_text(obj.pk)])
+            return mark_safe("""<a href="{url}">{text}</a>""".format(
+                url=url,
+                text=("가게 평점, 문의글 보러가기"),
+            ))
+        return ("(save and continue editing to create a link)")
+    get_edit_link.short_description = ("가게 자세히 보기")
+    get_edit_link.allow_tags = True
+
 
 @admin.register(get_user_model())
 class AdminUser(AuthUserAdmin):
 
-    list_display = ('id', 'store_image','username','email','user_phone', 'is_active', 'is_staff','item_ctn')
+    list_display = ('id', store_image,'username','email','user_phone', 'is_active', 'is_staff','item_ctn')
     list_display_links = ['username']
     list_editable = ('is_active', 'is_staff')
     list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups', 'date_joined')
     search_fields = ('username', 'email')
-    actions = ('user_acitve','create_storeprofile')
+    actions = ('user_acitve','user_disable')
     ordering = ('-is_superuser', '-is_staff', '-is_active')
     list_per_page = 20
 
-    inlines = (ProfileInline, StoreProfileInline)
+    inlines = (ProfileInline, StoreProfileInline,)
     fieldsets = (
         # (('유저 프로필과 가게 프로필'),{'fields':(inlines)}),
         (('개인 정보'), {'fields': ('username', 'email')}),
@@ -56,11 +97,6 @@ class AdminUser(AuthUserAdmin):
         }),
     )
 
-    def has_add_permission(self, request):
-        return False
-
-    def has_delete_permission(self, request, obj=None):
-        return False
 
     def get_queryset(self, request):
         queryset = super().get_queryset(request)
@@ -71,18 +107,10 @@ class AdminUser(AuthUserAdmin):
 
     def get_readonly_fields(self, request, obj=None):
         if obj:
-            return ['username', 'email', 'last_login', 'date_joined']
+            return ['username', 'password', 'email', 'last_login', 'date_joined']
         else:
             return []
 
-    def store_image(self, obj):
-        return mark_safe('<img src="{url}" width="{width}" height={height} />'.format(
-            url = obj.storeprofile.photo.url,
-            width=100,
-            height=100,
-            )
-    )
-    store_image.short_description = '스토어 사진 뷰'
 
 
     def item_ctn(self, obj):
@@ -95,21 +123,43 @@ class AdminUser(AuthUserAdmin):
     user_phone.short_description = '연락처'
 
     def user_acitve(self, request, queryset):
-        for user in queryset:
-            user.is_active = True
-            user.save()
+        queryset.update(is_active = True)
         # self.message_user(request, 'hello world')
-    user_acitve.short_description = '유저 활성화하기'
+    user_acitve.short_description = '유저 활성화'
+
+    def user_disable(self, request, queryset):
+        queryset.update(is_active = False)
+    user_disable.short_description = '유저 비활성화'
 
 
     def create_storeprofile(self, request, queryset):
-        from store.models import StoreProfile
         for user in queryset:
             StoreProfile.objects.create(user=user, name=user.username + '의 가게')
     create_storeprofile.short_description = '스토어프로필 만들기'
 
 
+
+class StoreProfileAdmin(admin.ModelAdmin):
+    fields = ['name', store_image, 'photo', 'comment']
+    readonly_fields = [store_image]
+    inlines = [QuestionCommentAdmin, StoreGradeAdmin]
+
+    def get_model_perms(self, request):
+        """
+        Return empty perms dict thus hiding the model from admin index.
+        """
+        return {}
+
+admin.site.register(StoreProfile, StoreProfileAdmin)
+
 @admin.register(UserSession)
 class Session(admin.ModelAdmin):
     list_display = ['user', 'session_key', 'created_at']
+    list_display_links = ['user', 'session_key']
+    ordering = ['user']
 
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
--- store/admin.py
+++ store/admin.py
@@ -3,10 +3,10 @@ from django.contrib import admin
 from .models import QuestionComment, StoreGrade
 
 
-@admin.register(QuestionComment)
-class CommentAdmin(admin.ModelAdmin):
-	list_display = ['pk', 'parent_id', 'store_profile', 'author']
-
-@admin.register(StoreGrade)
-class StoreGradeAdmin(admin.ModelAdmin):
-	list_display = ['pk', 'author', 'rating']
+# @admin.register(QuestionComment)
+# class CommentAdmin(admin.ModelAdmin):
+# 	list_display = ['pk', 'parent_id', 'store_profile', 'author']
+#
+# @admin.register(StoreGrade)
+# class StoreGradeAdmin(admin.ModelAdmin):
+# 	list_display = ['pk', 'author', 'rating']
