b49b19453f5bd098b1c18c0893b807902e827b84
thflthdi <thflthdi@gmail.com>
2019-10-14 23:17:15 +0900

    add: customer cbv로 변경 완료

--- customer/views.py
+++ customer/views.py
@@ -3,74 +3,119 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
 
 # Create your views here.
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, TemplateView, UpdateView, DeleteView, CreateView
 from hitcount.views import HitCountDetailView
-
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from .forms import CustomerAskForm
 from .models import CustomerFAQ, CustomerAsk, CustomerNotice
+# def customer(request):
+#     return render(request, 'customer/customer.html')
+
+# def customer_faq(request):
+#     faqs = CustomerFAQ.objects.all()
+
+#     return render(request, 'customer/customer_faq.html', {
+#         'faqs': faqs
+#     })
+
+class CustomerFAQView(ListView):
+    template_name = "customer/customer_faq.html"
+    model = CustomerFAQ
+    context_object_name = "faqs"
 
 
-def customer(request):
-    return render(request, 'customer/customer.html')
+# @login_required
+# def customer_ask(request):
 
-def customer_faq(request):
-    faqs = CustomerFAQ.objects.all()
+#     asks = CustomerAsk.objects.filter(author=request.user)
 
-    return render(request, 'customer/customer_faq.html', {
-        'faqs': faqs
-    })
+#     return render(request, 'customer/customer_ask.html', {
+#         'asks': asks
+#     })
 
-@login_required
-def customer_ask(request):
+@method_decorator(login_required, name='dispatch')
+class CustomerAskListView(ListView):
+    model = CustomerAsk
+    context_object_name = "asks"
+    template_name = "customer/customer_ask.html"
 
-    asks = CustomerAsk.objects.filter(author=request.user)
+# @login_required
+# def customer_ask_new(request):
+#     form_cls = CustomerAskForm
 
-    return render(request, 'customer/customer_ask.html', {
-        'asks': asks
-    })
+#     if request.method == 'POST':
+#         form = form_cls(request.POST)
 
-@login_required
-def customer_ask_new(request):
-    form_cls = CustomerAskForm
+#         if form.is_valid():
+#             ask_form = form.save(commit=False)
+#             ask_form.author = request.user
+#             ask_form.save()
+#         return redirect('customer:customer_ask')
+#     else :
+#         form = form_cls
+#     return render(request, 'customer/customer_ask_new.html', {
+#         'form':form
+#     })
 
-    if request.method == 'POST':
-        form = form_cls(request.POST)
+@method_decorator(login_required, name='dispatch')
+class CustomerAskCreateView(CreateView):
+    template_name = "customer/customer_ask_new.html"
+    model = CustomerAsk
+    form_class = CustomerAskForm
 
-        if form.is_valid():
-            ask_form = form.save(commit=False)
-            ask_form.author = request.user
-            ask_form.save()
+    def form_valid(self, form):
+        ask_form = form.save(commit=False)
+        ask_form.author = self.request.user
+        ask_form.save()
         return redirect('customer:customer_ask')
-    else :
-        form = form_cls
-    return render(request, 'customer/customer_ask_new.html', {
-        'form':form
-    })
 
-@login_required
-def customer_ask_edit(request, ask_id):
-    form_cls = CustomerAskForm
-    ask_post = get_object_or_404(CustomerAsk, pk=ask_id)
-    if request.method == 'POST':
-        form = form_cls(request.POST, instance=ask_post)
+# @login_required
+# def customer_ask_edit(request, ask_id):
+#     form_cls = CustomerAskForm
+#     ask_post = get_object_or_404(CustomerAsk, pk=ask_id)
+#     if request.method == 'POST':
+#         form = form_cls(request.POST, instance=ask_post)
 
-        if form.is_valid():
-            ask_form = form.save(commit=False)
-            ask_form.author = request.user
-            ask_form.save()
+#         if form.is_valid():
+#             ask_form = form.save(commit=False)
+#             ask_form.author = request.user
+#             ask_form.save()
+#         return redirect('customer:customer_ask')
+#     else :
+#         form = form_cls(instance=ask_post)
+#     return render(request, 'customer/customer_ask_new.html', {
+#         'form': form
+#     })
+
+@method_decorator(login_required, name='dispatch')
+class CustomerAskEditView(UpdateView):
+    model = CustomerAsk
+    form_class = CustomerAskForm
+    template_name = "customer/customer_ask_new.html"
+    pk_url_kwarg = 'ask_id'
+    context_object_name = "ask"
+    def form_valid(self, form):
+        ask_form = form.save(commit=False)
+        ask_form.author = self.request.user
+        ask_form.save()
         return redirect('customer:customer_ask')
-    else :
-        form = form_cls(instance=ask_post)
-    return render(request, 'customer/customer_ask_new.html', {
-        'form': form
-    })
 
-@login_required
-def customer_ask_detail(request, ask_id):
-    ask_post = get_object_or_404(CustomerAsk, pk=ask_id)
-    return render(request, 'customer/customer_ask_detail.html', {
-        'ask': ask_post,
-    })
+# @login_required
+# def customer_ask_detail(request, ask_id):
+    # ask_post = get_object_or_404(CustomerAsk, pk=ask_id)
+    # return render(request, 'customer/customer_ask_detail.html', {
+    #     'ask': ask_post,
+    # })
+
+@method_decorator(login_required, name='dispatch')
+class CustomerAskDetailView(DetailView):
+    template_name = "customer/customer_ask_detail.html"
+    model = CustomerAsk
+    pk_url_kwarg = 'ask_id'
+    context_object_name = "ask"
+
+
 
 class CustomerNoticeList(ListView):
     model = CustomerNotice
