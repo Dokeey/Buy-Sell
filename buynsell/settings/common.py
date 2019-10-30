780d8511178cc2c9f4aaa347543e6c3e7dccb97c
dokeey <ehdgnv@naver.com>
2019-10-30 19:04:57 +0900

    mod: debug 모드 false

--- buynsell/settings/common.py
+++ buynsell/settings/common.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 SECRET_KEY = '_ve!tya9hxp$#n$md(#@11o_x%de8dagvbm9j$+5z)$%&l%utx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
