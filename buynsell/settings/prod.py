1709ee7975afa3de7a368d9d5b252bb2a26f0a54
dokeey <ehdgnv@naver.com>
2020-02-04 18:53:33 +0900

    add: AWS_KEY 보안강화

--- buynsell/settings/prod.py
+++ buynsell/settings/prod.py
@@ -3,11 +3,11 @@ from .common import *
 
 # AWS Setting
 AWS_REGION = 'ap-northeast-2'
-AWS_STORAGE_BUCKET_NAME = 'elasticbeanstalk-ap-northeast-2-109247251546'
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME') # 'elasticbeanstalk-ap-northeast-2-109247251546'
 AWS_QUERYSTRING_AUTH = False
 AWS_S3_HOST = 's3.%s.amazonaws.com' % AWS_REGION
-AWS_ACCESS_KEY_ID = 'AKIARS35EYRNCE5R7HWL'
-AWS_SECRET_ACCESS_KEY = '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'
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID') # 'AKIARS35EYRNCE5R7HWL'
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY') # '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'
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
 # Static Setting
